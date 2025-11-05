89f7f3ddb5d797f2fa336713054eff28184cd0ee
HarshvardhanTakale <harshvardhantakale18@gmail.com>
2025-11-05 13:39:34 +0530

    Redux Toolkit using Shoping cart Application
    Redux Toolkit using Shoping cart Application

---
+++ ShopingCart using Redux/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Counter from './componants/Counter'
+import Products from './componants/Products';
+
+const App = () => {
+  return (
+    <div>
+      <Products/>
+    </div>
+  )
+}
+
+export default App
